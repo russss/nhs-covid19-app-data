4dd65632c0242df6d9ce8012c8af1a5471dfdb5f
Russ Garrett <russ@garrett.co.uk>
2021-12-27 13:56:19 +0000

    Add test availability, reliability improvements

--- fetch.py
+++ fetch.py
@@ -1,7 +1,9 @@
 import sqlite3
 import arrow
 import requests
+from requests.exceptions import HTTPError
 import logging
+import json
 
 from api import get_daily_file, get_two_hourly_file, get_risky_venues
 
@@ -41,12 +43,26 @@ def init_db():
               )"""
     )
 
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS walk_in_pcr_availability (
+                    date INTEGER NOT NULL,
+                    availability TEXT NOT NULL
+
+    )"""
+    )
+
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS home_test_availability (
+                    date INTEGER NOT NULL,
+                    pcr_keyworker TEXT NOT NULL,
+                    pcr_public TEXT NOT NULL,
+                    lfd_public TEXT NOT NULL
+    )"""
+    )
+
     c.execute("""CREATE TABLE IF NOT EXISTS last_update (end_timestamp INTEGER)""")
 
     conn.commit()
-
-
-init_db()
 
 
 def get_timestamp():
@@ -115,22 +131,27 @@ def insert_risky_venue(venue):
     return True
 
 
-timestamp = get_timestamp()
-log.info("Fetching keys from timestamp %s...", timestamp)
-
-while timestamp < arrow.utcnow().shift(hours=-2):
-    if timestamp < arrow.utcnow().shift(days=-1):
-        data = get_daily_file(timestamp)
-        save_timestamp(timestamp.shift(days=1))
-    else:
-        data = get_two_hourly_file(timestamp)
-        save_timestamp(timestamp.shift(hours=2))
-
-    insert_exposure_data(data)
-    conn.commit()
+def import_exposure_data():
     timestamp = get_timestamp()
+    log.info("Fetching keys from timestamp %s...", timestamp)
 
-log.info("Fetched keys to timestamp %s", timestamp)
+    while timestamp < arrow.utcnow().shift(hours=-2):
+        try:
+            if timestamp < arrow.utcnow().shift(days=-1):
+                data = get_daily_file(timestamp)
+                save_timestamp(timestamp.shift(days=1))
+            else:
+                data = get_two_hourly_file(timestamp)
+                save_timestamp(timestamp.shift(hours=2))
+        except HTTPError:
+            log.exception("Error fetching file")
+            return
+
+        insert_exposure_data(data)
+        conn.commit()
+        timestamp = get_timestamp()
+
+    log.info("Fetched keys to timestamp %s", timestamp)
 
 
 def import_risky_venues():
@@ -146,25 +167,95 @@ def import_risky_venues():
     log.info("Saw %s venues, %s new.", seen, new)
 
 
-import_risky_venues()
-
-
 def import_exposure_configuration():
     url = "https://distribution-te-prod.prod.svc-test-trace.nhs.uk/distribution/exposure-configuration"
     config = requests.get(url).text
 
-    c.execute("SELECT configuration FROM exposure_configuration ORDER BY date DESC LIMIT 1")
+    c.execute(
+        "SELECT configuration FROM exposure_configuration ORDER BY date DESC LIMIT 1"
+    )
     res = c.fetchone()
     if res and res[0] == config:
         return
 
-    c.execute("INSERT INTO exposure_configuration (date, configuration) VALUES (?, ?)",
-              (arrow.now().timestamp, config))
+    c.execute(
+        "INSERT INTO exposure_configuration (date, configuration) VALUES (?, ?)",
+        (arrow.now().timestamp, config),
+    )
 
     conn.commit()
     log.info("Inserted updated exposure configuration")
 
 
-import_exposure_configuration()
+def import_test_availability():
+    s = requests.Session()
+    s.headers.update(
+        {"Origin": "https://self-referral.test-for-coronavirus.service.gov.uk"}
+    )
 
+    # PCR walk-in availability
+    res = s.get(
+        "https://api-prd-prd-1-ibt.prd.ibt.test-and-trace.nhs.uk/apptbooking/testcentres/availabilitysummary"
+    )
+
+    res.raise_for_status()
+    walk_in = res.json()
+
+    c.execute("SELECT availability FROM walk_in_pcr_availability ORDER BY date DESC")
+    res = c.fetchone()
+    if not res or json.loads(res[0])['availability'] != walk_in['availability']:
+        log.info("Inserting updated walk-in PCR availability")
+        c.execute(
+            "INSERT INTO walk_in_pcr_availability (date, availability) VALUES (?, ?)",
+            (arrow.now().timestamp, walk_in),
+        )
+        conn.commit()
+
+    # PCR home tests, keyworkers
+    res = s.get(
+        "https://api-prd-prd-1-ibt.prd.ibt.test-and-trace.nhs.uk/ser/app/homeOrderAvailabilityStatus/antigen-keyworkers"
+    )
+
+    res.raise_for_status()
+    pcr_keyworker = res.json()["status"]
+
+    # PCR home tests, public
+    res = s.get(
+        "https://api-prd-prd-1-ibt.prd.ibt.test-and-trace.nhs.uk/ser/app/homeOrderAvailabilityStatus/antigen-public"
+    )
+
+    res.raise_for_status()
+    pcr_public = res.json()["status"]
+
+    # origin: https://test-for-coronavirus.service.gov.uk
+    # LFD tests, public
+    res = s.get(
+        "https://api.test-for-coronavirus.service.gov.uk/ser/app/homeOrderAvailabilityStatus/lfd3-public"
+    )
+    res.raise_for_status()
+    lfd_public = res.json()["status"]
+
+    c.execute(
+        "SELECT pcr_keyworker, pcr_public, lfd_public FROM home_test_availability ORDER BY date DESC"
+    )
+    res = c.fetchone()
+    if (
+        not res
+        or res[0] != pcr_keyworker
+        or res[1] != pcr_public
+        or res[2] != lfd_public
+    ):
+        log.info("Inserting updated home test availability")
+        c.execute(
+            "INSERT INTO home_test_availability (date, pcr_keyworker, pcr_public, lfd_public) VALUES (?, ?, ?, ?)",
+            (arrow.now().timestamp, pcr_keyworker, pcr_public, lfd_public),
+        )
+        conn.commit()
+
+
+init_db()
+import_exposure_data()
+import_risky_venues()
+import_exposure_configuration()
+import_test_availability()
 log.info("Run finished")
